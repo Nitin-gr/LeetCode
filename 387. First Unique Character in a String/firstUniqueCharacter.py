ca79e4fe6a0d29d8bb68446cc5067bade79a2f81
nitin-gr <nitin.gr@icloud.com>
2023-11-18 21:26:58 +0530

    completed on 18/11

---
+++ 387. First Unique Character in a String/firstUniqueCharacter.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def firstUniqChar(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        for i in range(len(s)):
+            if s.count(s[i])==1:
+                return i
+        return -1        
+    
+#better runtime
+class Solution(object):
+    def firstUniqChar(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        for i in range(len(s)):
+            if s[i] not in s[i+1:] and s[i] not in s[:i]:
+                return i
+        return -1        
